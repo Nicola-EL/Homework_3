d3611f3f992576b4aa5468bddfcc320d9a983159
Nicola-EL <eslis2004@gmail.com>
2024-07-03 22:26:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+hw_quantity = "12" #hw=homework
+time_quantity = "1.5"
+name_of_course = "Python"
+print(1.5/12) #вычисляем среднее время
+time_for_one_lesson = "0.125"
+print("курс",name_of_course,"всего задач", hw_quantity, "затраченно часов", time_quantity, "среднее время выполнения", time_for_one_lesson)
+
+
+
+
